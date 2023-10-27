6318ad97d7dd53fe98b6b28a5886313723e5f761
Viacheslav <vya4eslav.tka4enko@gmail.com>
2023-10-27 23:44:35 +0100

    first

---
+++ main.py
@@ -0,0 +1,24 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+#
+question_bank = []
+for question in question_data:
+    question_text = question['question']
+    question_answer = question['correct_answer']
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
+
+# question_1 = Question('hello','do')
+# print(question_bank)
+
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've complete the quiz")
+print(f"Your finul score was: {quiz.score}/ {len(question_bank)}")
---
+++ question_model.py
@@ -0,0 +1,9 @@
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+
+# question_1 = Question("How are you", "I'm fine, Thank you")
+
+# print(question_1.answer)
